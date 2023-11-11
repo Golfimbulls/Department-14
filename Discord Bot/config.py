d428643ef259631b7bf942946d24daa3d04accf8
Golfimbulls <150305582+Golfimbulls@users.noreply.github.com>
2023-11-11 09:40:15 -0800

    Add files via upload

---
+++ Discord Bot/config.py
@@ -0,0 +1,26 @@
+# config.py
+
+# Discord Bot Configuration
+COMMAND_PREFIX = '!'  # Prefix for commands
+TOKEN_FILE = 'token.txt'  # File to store the Discord bot token
+
+# Logging Configuration
+LOG_CHANNEL_ID = 123456789012345678  # Replace with your actual channel ID for logging
+
+# Bot Features Configuration
+# Add any specific IDs or settings related to the bot's features here
+# For example, if you have specific roles or channels that the bot interacts with
+
+# Ticket System Configuration (if implemented)
+TICKET_CATEGORY_ID = 987654321098765432  # Replace with your actual category ID for ticket channels
+
+# Moderation Features
+# If you have moderation features, you can configure them here
+# For example, role IDs for moderators, banned words list, etc.
+
+# API Keys (if your bot interacts with external services)
+# SOME_API_KEY = 'your_api_key_here'
+
+# Other Constants
+# You can define other constants that might be used across your bot
+# For example, welcome messages, default responses, etc.
