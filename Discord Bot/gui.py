d1ca184196b834bcea3c910c173256761132b6b1
RhythmicCarnage <admin@prohibited.tv>
2023-11-17 17:08:04 -0700

    Working GUI with system uptime counter

--- Discord Bot/gui.py
+++ Discord Bot/gui.py
@@ -25,133 +25,212 @@ Each widget (button, label, entry field) is equipped with tooltips for additiona
 The script uses the tkinter library for creating the GUI components.
 """
 
+# Import necessary modules from tkinter for creating the GUI
 import tkinter as tk
 from tkinter import ttk, scrolledtext, simpledialog, filedialog, Menu, Label, Entry, Button, messagebox
-from PIL import Image, ImageTk
-import threading
-import token_manager  # Import the token manager
-import event_handlers  # Import event handlers
+
+from PIL import Image, ImageTk # PIL is used for image processing - useful for handling images in the GUI
+import threading # threading is used for running tasks in separate threads
+import token_manager # token_manager is a custom module for handling the bot's token
+import event_handlers # event_handlers is a custom module for handling events in the application
+# sys and os are standard Python modules for system and operating system functionalities
 import sys
 import os
-import time
-
+import time # time module is used for time-related functions
+import psutil
 
 class Tooltip:
     """
     Create a tooltip for a given widget.
+
+    This class is used to create small pop-up boxes (tooltips) that display
+    information when the user hovers over a widget. It enhances the user
+    experience by providing helpful hints or additional information about
+    the GUI elements.
     """
 
     def __init__(self, widget, text):
-        self.waittime = 500     # Milliseconds
-        self.wraplength = 180   # Pixels
-        self.widget = widget
-        self.text = text
-        self.widget.bind("<Enter>", self.enter)
-        self.widget.bind("<Leave>", self.leave)
-        self.widget.bind("<ButtonPress>", self.leave)
-        self.id = None
-        self.tw = None
+        # Initialization function that sets up the tooltip
+        self.waittime = 500     # Milliseconds to wait before showing the tooltip
+        self.wraplength = 180   # Width in pixels before wrapping text to a new line
+        self.widget = widget    # The widget the tooltip is attached to
+        self.text = text        # The text displayed in the tooltip
+        # Bind mouse events to the widget to show and hide the tooltip
+        self.widget.bind("<Enter>", self.enter)  # Mouse enters the widget area
+        self.widget.bind("<Leave>", self.leave)  # Mouse leaves the widget area
+        self.widget.bind("<ButtonPress>", self.leave)  # Mouse clicks the widget
+        self.id = None          # Used to keep track of the delay timer
+        self.tw = None          # The tooltip window itself
 
     def enter(self, event=None):
-        self.schedule()
+        # Called when mouse enters the widget area
+        self.schedule()         # Schedule the tooltip to appear
 
     def leave(self, event=None):
-        self.unschedule()
-        self.hidetip()
+        # Called when mouse leaves the widget area or clicks on the widget
+        self.unschedule()       # Cancel the tooltip if it's scheduled to appear
+        self.hidetip()          # Hide the tooltip
 
     def schedule(self):
-        self.unschedule()
-        self.id = self.widget.after(self.waittime, self.showtip)
+        # Schedule the tooltip to appear after a delay
+        self.unschedule()       # Ensure no other tooltip is scheduled
+        self.id = self.widget.after(self.waittime, self.showtip)  # Set a timer to show the tooltip
 
     def unschedule(self):
+        # Cancel any scheduled tooltip
         id = self.id
         self.id = None
         if id:
-            self.widget.after_cancel(id)
+            self.widget.after_cancel(id)  # Cancel the timer
 
     def showtip(self, event=None):
-        x, y, cx, cy = self.widget.bbox("insert")
-        x += self.widget.winfo_rootx() + 25
-        y += self.widget.winfo_rooty() + 20
+        # Show the tooltip
+        x, y, cx, cy = self.widget.bbox("insert")  # Get the position of the widget
+        x += self.widget.winfo_rootx() + 25        # Adjust the x-coordinate
+        y += self.widget.winfo_rooty() + 20        # Adjust the y-coordinate
+        # Create a new top-level window for the tooltip
         self.tw = tk.Toplevel(self.widget)
-        self.tw.wm_overrideredirect(True)
-        self.tw.wm_geometry(f"+{x}+{y}")
+        self.tw.wm_overrideredirect(True)          # Remove window decorations
+        self.tw.wm_geometry(f"+{x}+{y}")           # Set the position of the tooltip
+        # Create a label inside the tooltip window to show the text
         label = tk.Label(self.tw, text=self.text, justify='left',
                          background="#ffffff", relief='solid', borderwidth=1,
                          wraplength=self.wraplength)
-        label.pack(ipadx=1)
+        label.pack(ipadx=1)    # Pack the label into the window
 
     def hidetip(self):
+        # Hide the tooltip
         tw = self.tw
         self.tw = None
         if tw:
-            tw.destroy()
-
+            tw.destroy()  # Destroy the tooltip window
 
 class BotGUI(tk.Tk):
+    # This class represents the main window of the Discord Bot Control Panel GUI
     def __init__(self, update_bot_status_callback=None):
-        super().__init__()
-        self.title("Discord Bot Control Panel")
-        self.geometry("700x700")
+        super().__init__()  # Initialize the superclass (tk.Tk) to set up the window
+        self.title("Discord Bot Control Panel")  # Set the window title
+        self.geometry("700x700")  # Set the window size to 700x700 pixels
 
-        # Define the background color attribute
+        # Define the color scheme for the GUI
         self.bg_color = "#000000"  # Black background
+        self.text_color = "#00FF00"  # Green text for contrast against the black background
+        self.button_color = "#004d00"  # Dark green color for buttons
+        self.button_text_color = "#00FF00"  # Green text on buttons for readability
 
-        self.configure(bg=self.bg_color)  # Use the bg_color attribute
-        self.update_bot_status_callback = update_bot_status_callback
+        self.configure(bg=self.bg_color)  # Set the background color for the window
+        self.update_bot_status_callback = update_bot_status_callback  # Callback function to update bot status
 
-        self.setup_menu_bar()
-        self.setup_frames_and_panels()
-        self.add_tooltips()
-        self.initialized = True
+        # Set up various components of the GUI
+        self.setup_menu_bar()  # Set up the menu bar at the top of the window
+        self.setup_frames_and_panels()  # Set up the main frames and panels in the GUI
+        self.add_tooltips()  # Add tooltips to provide additional information for widgets
+        self.start_time = time.time()  # Record the start time for uptime calculation
+        self.update_performance_metrics()  # Start updating performance metrics
+        self.initialized = True  # Mark that initialization is complete
 
     def setup_menu_bar(self):
-        menu_bar = Menu(self, bg="#003300", fg="#00FF00")
-        self.config(menu=menu_bar)
+        # Set up the menu bar for the application
+        menu_bar = Menu(self, bg="#003300", fg="#00FF00")  # Create a menu bar with dark green background
+        self.config(menu=menu_bar)  # Add the menu bar to the main window
 
+        # Create a 'File' menu
         file_menu = Menu(menu_bar, tearoff=0, bg="#003300", fg="#00FF00")
         menu_bar.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="View Token", command=self.view_token)
-        file_menu.add_command(label="Edit Token", command=self.edit_token)
-        file_menu.add_command(label="Save Token", command=self.save_token)
-        file_menu.add_separator()
         file_menu.add_command(label="Save Logs", command=self.save_log)
+        file_menu.add_command(label="Exit", command=self.quit)
+
+        # Create a 'Bot Token' menu
+        token_menu = Menu(menu_bar, tearoff=0, bg="#003300", fg="#00FF00")
+        menu_bar.add_cascade(label="Bot Token", menu=token_menu)
+        token_menu.add_command(label="View Token", command=self.view_token)
+        token_menu.add_command(label="Edit Token", command=self.edit_token)
+        token_menu.add_command(label="Save Token", command=self.save_token)
+
+        # Create a 'Bot Management' menu
+        bot_management_menu = Menu(menu_bar, tearoff=0, bg="#003300", fg="#00FF00")
+        menu_bar.add_cascade(label="Bot Management", menu=bot_management_menu)
+        bot_management_menu.add_command(label="Start Bot", command=self.check_and_start_bot)
+        bot_management_menu.add_command(label="Stop Bot", command=self.stop_bot)
+        bot_management_menu.add_command(label="Restart Bot", command=self.restart_bot)
+
+        # Create a 'Settings' menu
+        settings_menu = Menu(menu_bar, tearoff=0, bg="#003300", fg="#00FF00")
+        menu_bar.add_cascade(label="Settings", menu=settings_menu)
+        settings_menu.add_command(label="General Settings", command=self.open_general_settings)
+        settings_menu.add_command(label="Moderation Settings", command=self.open_moderation_settings)
+
+        # Create a 'Help' menu
+        help_menu = Menu(menu_bar, tearoff=0, bg="#003300", fg="#00FF00")
+        menu_bar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="Documentation", command=self.open_documentation)
+        help_menu.add_command(label="About", command=self.open_about)
+        
+    # Placeholder methods for new menu actions
+    def stop_bot(self):
+        pass  # Logic to stop the bot
+
+    def restart_bot(self):
+        pass  # Logic to restart the bot
+
+    def open_general_settings(self):
+        pass  # Logic to open general settings dialog
+
+    def open_moderation_settings(self):
+        pass  # Logic to open moderation settings dialog
+
+    def open_documentation(self):
+        pass  # Logic to open documentation
+
+    def open_about(self):
+        pass  # Logic to display information about the application
+    
+    def update_performance_metrics(self):
+        # Calculate uptime
+        uptime_seconds = time.time() - self.start_time
+        uptime_str = time.strftime('%Hh %Mm %Ss', time.gmtime(uptime_seconds))
+
+        cpu_usage = psutil.cpu_percent() # Get CPU usage
+        self.performance_label.config(text=f"Uptime: {uptime_str}, CPU Usage: {cpu_usage}%") # Update the label
+
+        # Schedule this method to be called again after 1000 milliseconds (1 second)
+        self.after(1000, self.update_performance_metrics)
 
     def setup_frames_and_panels(self):
-        self.setup_bot_control_frame()
-        self.setup_command_frame()
-        self.setup_notebook()
-        self.setup_button_frame()
+        # This method organizes the main sections (frames and panels) of the GUI.
+        self.setup_bot_control_frame()  # Sets up the 'Bot Control Frame' which is a dedicated section in the GUI for displaying the bot's status, connected servers, and performance metrics.
+        self.setup_command_frame()  # Initializes the 'Command Frame'. This is where the user can input and send commands to the Discord bot. It typically includes a text entry field and a send button.
+        self.setup_channel_id_frame()  # Creates a frame for managing channel IDs. This is important for specifying which Discord channels the bot should log messages to or operate in. It is important to set this up before the notebook because it might be referenced in the tabs of the notebook.
+        self.setup_notebook()  # Prepares a 'Notebook' widget, which is a tabbed control element in Tkinter. It allows the GUI to have multiple tabs for different purposes, such as logs, user/server lists, and activity feeds.
+        self.setup_button_frame()  # Establishes a 'Button Frame' which contains various buttons for actions like starting the bot, toggling auto-moderation, and other functions. This is where you would add buttons that perform global actions related to the bot.
 
     def setup_bot_control_frame(self):
-        # Bot Control Frame
+        # This method creates and configures the 'Bot Control Frame' within the GUI. This frame is dedicated to displaying various information about the bot's operational status.
+
+        # Create a frame to hold bot control elements, with a black background.
         self.bot_control_frame = tk.Frame(self, bg="#000000")
         self.bot_control_frame.pack(padx=10, pady=5)
 
-        # Bot Status Label
-        self.bot_status_label = Label(
-            self.bot_control_frame, text="Bot Status: Unknown", bg="#000000", fg="#00FF00")
+        # Bot Status Label: Shows the current status of the bot (e.g., online, offline). Initially set to "Unknown" until the bot's status is updated.
+        self.bot_status_label = Label(self.bot_control_frame, text="Bot Status: Unknown", bg="#000000", fg="#00FF00")
         self.bot_status_label.pack(side=tk.LEFT, padx=5)
 
-        # Server Count Label
-        self.server_count_label = Label(
-            self.bot_control_frame, text="Connected Servers: 0", bg="#000000", fg="#00FF00")
+        # Server Count Label: Displays the number of servers the bot is currently connected to. Initially set to 0.
+        self.server_count_label = Label(self.bot_control_frame, text="Connected Servers: 0", bg="#000000", fg="#00FF00")
         self.server_count_label.pack(side=tk.LEFT, padx=5)
 
-        # Bot Performance Metrics
-        self.performance_label = Label(
-            self.bot_control_frame, text="Uptime: 0h 0m 0s, CPU Usage: 0%", bg="#000000", fg="#00FF00")
+        # Bot Performance Metrics: Shows key performance indicators such as uptime and CPU usage. Initially shows zero usage.
+        self.performance_label = Label(self.bot_control_frame, text="Uptime: 0h 0m 0s, CPU Usage: 0%", bg="#000000", fg="#00FF00")
         self.performance_label.pack(side=tk.LEFT, padx=5)
 
-        # Bot Status Control (Dropdown for status change)
+        # Bot Status Control: A dropdown menu to change the bot's status (e.g., online, idle, invisible). This allows dynamic control over the bot's operational status.
         self.status_var = tk.StringVar(self)
         self.status_options = ['online', 'idle', 'invisible']
-        self.status_menu = tk.OptionMenu(
-            self.bot_control_frame, self.status_var, *self.status_options, command=self.change_bot_status)
+        self.status_menu = tk.OptionMenu(self.bot_control_frame, self.status_var, *self.status_options, command=self.change_bot_status)
         self.status_menu.config(bg="#004d00", fg="#00FF00")
         self.status_menu.pack(side=tk.LEFT, padx=5, pady=5)
 
-        # Additional widgets can be added here as needed
+        # Additional widgets can be added to this frame as needed to extend functionality.
 
     def setup_command_frame(self):
         self.command_frame = tk.Frame(self, bg="#000000")
@@ -282,16 +361,6 @@ class BotGUI(tk.Tk):
         self.button_frame = tk.Frame(self, bg=self.bg_color)
         self.button_frame.pack(padx=10, pady=5)
 
-        # Start Bot Button
-        self.start_bot_button = tk.Button(
-            self.button_frame,
-            text="Start Bot",
-            command=self.check_and_start_bot,
-            bg=self.button_color,
-            fg=self.button_text_color
-        )
-        self.start_bot_button.pack(side=tk.LEFT, padx=5, pady=5)
-
         # Auto Moderation Toggle Button
         self.auto_mod_button = tk.Button(
             self.button_frame,
@@ -337,7 +406,6 @@ class BotGUI(tk.Tk):
                 "Enter the ID of the Discord channel for the bot to operate in.")
         Tooltip(self.set_channel_button,
                 "Set the bot to use the specified channel.")
-        Tooltip(self.start_bot_button, "Start the Discord bot.")
         Tooltip(self.auto_mod_button, "Toggle automatic moderation features.")
 
     def check_and_start_bot(self):
@@ -540,7 +608,7 @@ def show_splash_screen():
                          splash_screen_height / img_height)
     new_size = (int(img_width * scaling_factor),
                 int(img_height * scaling_factor))
-    splash_image = splash_image.resize(new_size, Image.Resampling.LANCZOS)
+    splash_image = splash_image.resize(new_size, Image.LANCZOS)
 
     splash_photo = ImageTk.PhotoImage(splash_image)
 
--- Discord Bot/token_manager.py
+++ Discord Bot/token_manager.py
@@ -1,19 +1,36 @@
 import os
+import logging
+
+# Initialize logging
+logger = logging.getLogger(__name__)
 
 TOKEN_FILE = 'bot_token.txt'
 
-def save_token(token):
-    """Save the token to a file."""
-    with open(TOKEN_FILE, 'w') as file:
-        file.write(token)
+def save_token(token, filepath=TOKEN_FILE):
+    """Save the token to a file. Validate the token format before saving."""
+    try:
+        # Simple token format validation (you can enhance this as per your needs)
+        if not token or not isinstance(token, str):
+            raise ValueError("Invalid token format.")
 
-def load_token():
+        with open(filepath, 'w') as file:
+            file.write(token)
+        logger.info("Token successfully saved.")
+    except Exception as e:
+        logger.error(f"Error saving token: {e}")
+
+def load_token(filepath=TOKEN_FILE):
     """Load the token from a file, or return None if it doesn't exist."""
-    if os.path.exists(TOKEN_FILE):
-        with open(TOKEN_FILE, 'r') as file:
-            return file.read().strip()
-    else:
-        # Create an empty file if it doesn't exist
-        with open(TOKEN_FILE, 'w') as file:
-            pass
-    return None
+    try:
+        if os.path.exists(filepath):
+            with open(filepath, 'r') as file:
+                return file.read().strip()
+        else:
+            # Create an empty file if it doesn't exist
+            with open(filepath, 'w') as file:
+                pass
+        logger.info("Token file created.")
+        return None
+    except Exception as e:
+        logger.error(f"Error loading token: {e}")
+        return None
